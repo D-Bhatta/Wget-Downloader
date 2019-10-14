c4fd39f9223275a568af40e1d383f277c074167e
D-Bhatta <dbhatta1232@gmail.com>
2019-10-14 14:08:43 -0400

    Make it into a script

--- WgetDownloader.py
+++ WgetDownloader.py
@@ -3,57 +3,68 @@ class WgetDownloader(object):
     def __init__(self,filename1, filename2):
         self.filename1 = filename1
         self.filename2 = filename2
+        self.names = []
+        self.links = []
+        self.queries = []
     def open_files(self):
         '''Open files and read them into lists'''
         #open links file and read lines into list
         print("opening links file")
         with open(self.filename1) as f:
-            links = f.read().splitlines()
-            #print(links)
+            self.links = f.read().splitlines()
+            #print(self.links)
         #open names file and read lines into list
         print("opening name file")
         with open(self.filename2) as f:
-            names = f.read().splitlines()
-            #print(names)
-        return names, links
+            self.names = f.read().splitlines()
+            #print(self.names)
 
-    def clean_lists(self,names):
+    def clean_lists(self):
         '''clean the lists of strings'''
         #strip ['\\', '"', "'", '<', '>', '[',']',':','/','|', '?', '*']  from names
         print("stripping undesirable characters")
         #bad_characters = ['\\', '"', "'", '<', '>', '[',']',':','/','|', '?', '*']
         bad_characters = str.maketrans('\\"\'<>[]:/|?*.-', "______________")
-        names = [x.translate(bad_characters) for x in names]
-        #print(names)
-        return names
+        self.names = [x.translate(bad_characters) for x in self.names]
+        #print(self.names)
 
-    def check_len(self,list1, list2):
+    def check_len(self):
         #len of lists test
-        if(len(list1)!=len(list2)):
-            print("The length of the lists do not match. Check the data.")
+        if(len(self.names)!=len(self.links)):
+            print("The length of the lists do not match. Check the data.",
+            "names :{}".format(len(self.names)), 
+            "links : {}".format(len(self.links)))
+            exit()
+    
+    def check_list_empty(self):
+        if len(self.names)==0:
+            print("The names list is empty")
+            exit()
+        if len(self.links)==0:
+            print("The links list is empty")
             exit()
 
-    def query_builder(self,names, links):
+    def query_builder(self):
         #build query list
-        queries = []
         print("building query list")
-        for i in range(len(links)):
-            folder_name = names[i]
+        for i in range(len(self.links)):
+            folder_name = self.names[i]
             folder_name = '"' + folder_name + '"'
-            link_name = links[i]
+            link_name = self.links[i]
             init_query = "wget -E -H -k -K -p -e robots=off -nH -nd -P" + folder_name+ "\ "+ link_name
-            queries.append(init_query)
+            self.queries.append(init_query)
             #print(init_query)
         #print(queries)
-        return queries
+        self.queries
 
-    def wget_execution(self,queries):
+    def wget_execution(self):
         from os import system
         #loop wget execution
         print("About to start looping")
-        for i in range(len(queries)):
-            system(queries[i])
+        for i in range(len(self.queries)):
+            system(self.queries[i])
             print("finished no.{} loop".format(i))
+        print("expected loops:{}".format(len(self.queries)-1))
     
     def exit_function(self):
         #exit
@@ -62,5 +73,14 @@ class WgetDownloader(object):
     
     def download(self):
         '''downloads the files'''
+        #get the lists
         self.open_files()
+        #check if lists are empty
+        self.check_list_empty()
+        self.check_list_empty()
+        self.check_len()
+        self.clean_lists()
+        self.query_builder()
+        self.wget_execution()
+        self.exit_function()
         
--- Wget_downloader.py
+++ Wget_downloader.py
@@ -1,9 +1,15 @@
 import WgetDownloader
 
+def main(argv):
+    [lo_, hi_] = argv[1:3]
+    filename1, filename2 = str(lo_), str(hi_)
+    #filename1 = "links.txt"
+    #filename2 = "names.txt"
+    download = WgetDownloader.WgetDownloader(filename1,filename2)
+    download.download()
 
-filename1 = "links.txt"
-filename2 = "names.txt"
-
-#exit
-print("done")
-#check if the number of queriees is the same as the number of links and names:
+if __name__ == "__main__":
+    def _script_io():
+        from sys import argv
+        main(argv)
+    _script_io()
