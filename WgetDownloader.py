f383fc5ee63a05654382b8757abff5a6dd7357ca
D-Bhatta <dbhatta1232@gmail.com>
2019-11-03 12:50:03 -0500

    Added function to check if internet connection is avalable and to raise an error if wget returns an error. Deleted duplicate self.check_list_empty() call in self.download(), used triple single quotes on all comments to describe functions

--- WgetDownloader.py
+++ WgetDownloader.py
@@ -55,7 +55,7 @@ class WgetDownloader(object):
         #print(self.names)
 
     def check_len(self):
-        #len of lists test
+        '''len of lists test'''
         if(len(self.names)!=len(self.links)):
             print("The length of the lists do not match. Check the data.",
             "names :{}".format(len(self.names)), 
@@ -63,6 +63,7 @@ class WgetDownloader(object):
             exit()
     
     def check_list_empty(self):
+        '''check if lists are empty'''
         if len(self.names)==0:
             print("The names list is empty")
             exit()
@@ -71,29 +72,66 @@ class WgetDownloader(object):
             exit()
 
     def query_builder(self):
-        #build query list
+        '''build query list'''
         print("building query list")
         for i in range(len(self.links)):
             folder_name = self.names[i]
             folder_name = '"' + folder_name + '"'
             link_name = self.links[i]
-            init_query = "wget -E -H -k -K -p -e robots=off -nH -nd -P" + folder_name+ "\ "+ link_name
+            init_query = "wget -E -H -k -K -p -e robots=off -nH -nd -P" + folder_name+ "\ "+ link_name # pylint: disable=anomalous-backslash-in-string
             self.queries.append(init_query)
             #print(init_query)
         #print(queries)
         self.queries
+    
+    def check_network(self):
+        '''check network connection'''
+        import socket
+
+        def internet(host="8.8.8.8", port=53, timeout=3):
+            """
+            Host: 8.8.8.8 (google-public-dns-a.google.com)
+            OpenPort: 53/tcp
+            Service: domain (DNS/TCP)
+            """
+            try:
+                socket.setdefaulttimeout(timeout)
+                socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+                return True
+            except socket.error as ex:
+                print(ex)
+                return False
+        
+        internet()
 
     def wget_execution(self):
+        '''executes the queries: calls wget with link and folder name along with flags"-E -H -k -K -p -e robots=off -nH -nd -P" and link is stored in folder'''
+
+        #Define Exceptions
+        class Error(Exception):
+            """Base class for other exceptions"""
+            pass
+
+        class WgetError(Error):
+            """Raised when wget can't execute the query and returned status is non-zero(!0)"""
+            pass
+
+
         from os import system
         #loop wget execution
         print("About to start looping")
         for i in range(len(self.queries)):
-            system(self.queries[i])
-            print("finished no.{} loop".format(i))
+            try:
+                status = system(self.queries[i])
+                print("finished no.{} loop".format(i))
+                if status != 0:
+                    raise WgetError("Wget error")
+            except WgetError:
+                print("Wget couldn't download the link and returned status {}".format(status))
         print("expected loops:{}".format(len(self.queries)-1))
     
     def exit_function(self):
-        #exit
+        '''exit'''
         print("done")
         #check if the number of queriees is the same as the number of links and names
     
@@ -103,10 +141,10 @@ class WgetDownloader(object):
         self.open_files()
         #check if lists are empty
         self.check_list_empty()
-        self.check_list_empty()
         self.check_len()
         self.clean_lists()
         self.query_builder()
+        self.check_network()
         self.wget_execution()
         self.exit_function()
         
